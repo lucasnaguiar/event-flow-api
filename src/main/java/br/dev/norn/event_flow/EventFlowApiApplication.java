63a8af190cbd21b39cd0a18444ba0fcda2d17ce4
Lucas Aguiar <lucasbarbary@gmail.com>
2025-10-01 00:20:20 -0500

    add TODOs

--- src/main/java/br/dev/norn/event_flow/EventFlowApiApplication.java
+++ src/main/java/br/dev/norn/event_flow/EventFlowApiApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EventFlowApiApplication {
-
+//    TODO: Validacao de requisicoes
+//    TODO: Middleware de checagem de autenticação
+//
 	public static void main(String[] args) {
 		SpringApplication.run(EventFlowApiApplication.class, args);
 	}
